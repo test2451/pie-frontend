4adb38318c1c59a5d20a74e7b8e1169e3311dc4d
test2451 <79050952+test2451@users.noreply.github.com>
2021-02-16 02:12:37 +0530

    Update CardFooter.tsx

--- src/views/Pools/components/CardFooter.tsx
+++ src/views/Pools/components/CardFooter.tsx
@@ -9,7 +9,7 @@ import { CommunityTag, CoreTag, BinanceTag } from 'components/Tags'
 import { PoolCategory } from 'config/constants/types'
 
 const tags = {
-  [PoolCategory.BINANCE]: BinanceTag,
+  [PoolCategory.OKEX]: OkexTag,
   [PoolCategory.CORE]: CoreTag,
   [PoolCategory.COMMUNITY]: CommunityTag,
 }
