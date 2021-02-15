aee36199784cdc06878617459b24c1992d4bb18e
test2451 <79050952+test2451@users.noreply.github.com>
2021-02-16 01:43:40 +0530

    Update index.tsx

--- src/views/Profile/components/EditProfileModal/index.tsx
+++ src/views/Profile/components/EditProfileModal/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { InjectedModalProps, Modal } from '@pancakeswap-libs/uikit'
+import { InjectedModalProps, Modal } from '@pieswap-libs/uikit'
 import useI18n from 'hooks/useI18n'
 import useEditProfile, { Views } from './reducer'
 import StartView from './StartView'
 import PauseProfileView from './PauseProfileView'
 import ChangeProfilePicView from './ChangeProfilePicView'
-import ApproveCakeView from './ApproveCakeView'
+import ApprovePieView from './ApprovePieView'
 
 type EditProfileModalProps = InjectedModalProps
 
@@ -13,7 +13,7 @@ const viewTitle = {
   [Views.START]: { id: 999, label: 'Edit Profile' },
   [Views.CHANGE]: { id: 999, label: 'Change Profile Pic' },
   [Views.REMOVE]: { id: 999, label: 'Remove Profile Pic' },
-  [Views.APPROVE]: { id: 999, label: 'Approve CAKE' },
+  [Views.APPROVE]: { id: 999, label: 'Approve PIE' },
 }
 
 const EditProfileModal: React.FC<EditProfileModalProps> = ({ onDismiss }) => {
