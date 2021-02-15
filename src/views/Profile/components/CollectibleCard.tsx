afdd47bee50da5cbca50a2676e3fc97757670526
test2451 <79050952+test2451@users.noreply.github.com>
2021-02-16 01:27:51 +0530

    Update CollectibleCard.tsx

--- src/views/Profile/components/CollectibleCard.tsx
+++ src/views/Profile/components/CollectibleCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Heading, Text } from '@pancakeswap-libs/uikit'
+import { Heading, Text } from '@pieswap-libs/uikit'
 import { Nft } from 'config/constants/types'
 
 interface CollectibleCardProps {
