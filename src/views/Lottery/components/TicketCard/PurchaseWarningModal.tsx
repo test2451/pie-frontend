5e40566e0ca37667db2b9ae11bc3cd4600029219
test2451 <79050952+test2451@users.noreply.github.com>
2021-02-16 14:21:07 +0530

    Update PurchaseWarningModal.tsx

--- src/views/Lottery/components/TicketCard/PurchaseWarningModal.tsx
+++ src/views/Lottery/components/TicketCard/PurchaseWarningModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Modal } from '@pancakeswap-libs/uikit'
+import { Button, Modal } from '@pieswap-libs/uikit'
 import ModalActions from 'components/ModalActions'
 import styled from 'styled-components'
 import useI18n from 'hooks/useI18n'
@@ -12,7 +12,7 @@ const WarningModal: React.FC<{ onDismiss?: () => void }> = ({ onDismiss }) => {
       <TicketsList>
         {TranslateString(468, 'Lottery ticket purchases are final.')}
         <br />
-        {TranslateString(470, 'Your CAKE will not be returned to you after you spend it to buy tickets.')}
+        {TranslateString(470, 'Your PIE will not be returned to you after you spend it to buy tickets.')}
         <br />
         {TranslateString(472, 'Tickets are only valid for one lottery draw, and will be burned after the draw.')}
         <br />
