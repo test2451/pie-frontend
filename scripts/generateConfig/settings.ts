e949549281cfbf5b0154b8add9ed183e55925f7d
test2451 <79050952+test2451@users.noreply.github.com>
2021-02-17 00:15:13 +0530

    Update settings.ts

--- scripts/generateConfig/settings.ts
+++ scripts/generateConfig/settings.ts
@@ -1,6 +1,6 @@
 import { SettingsObject, SettingsType } from './types'
 
-const BASE_URL = 'https://pancake-config-api-chefkai.pancakeswap.vercel.app'
+const BASE_URL = 'https://pie-config-api-chefkai.pieswap.vercel.app'
 const settings: SettingsObject[] = [
   {
     name: 'ifos',
