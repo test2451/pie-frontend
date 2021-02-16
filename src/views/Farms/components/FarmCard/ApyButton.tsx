20452ad37adfc4eaf1b9d0723c74b765e473821a
test2451 <79050952+test2451@users.noreply.github.com>
2021-02-16 16:06:56 +0530

    Update ApyButton.tsx

--- src/views/Farms/components/FarmCard/ApyButton.tsx
+++ src/views/Farms/components/FarmCard/ApyButton.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import BigNumber from 'bignumber.js'
-import { CalculateIcon, IconButton, useModal } from '@pancakeswap-libs/uikit'
+import { CalculateIcon, IconButton, useModal } from '@pieswap-libs/uikit'
 import ApyCalculatorModal from './ApyCalculatorModal'
 
 export interface ApyButtonProps {
   lpLabel?: string
-  cakePrice?: BigNumber
+  piePrice?: BigNumber
   apy?: BigNumber
   addLiquidityUrl?: string
 }
 
-const ApyButton: React.FC<ApyButtonProps> = ({ lpLabel, cakePrice, apy, addLiquidityUrl }) => {
+const ApyButton: React.FC<ApyButtonProps> = ({ lpLabel, piePrice, apy, addLiquidityUrl }) => {
   const [onPresentApyModal] = useModal(
-    <ApyCalculatorModal lpLabel={lpLabel} cakePrice={cakePrice} apy={apy} addLiquidityUrl={addLiquidityUrl} />,
+    <ApyCalculatorModal lpLabel={lpLabel} cakePrice={piePrice} apy={apy} addLiquidityUrl={addLiquidityUrl} />,
   )
 
   return (
