04544c88fc320338838ea18fb5c60de93513dd9e
test2451 <79050952+test2451@users.noreply.github.com>
2021-02-16 16:30:09 +0530

    Update compoundApyHelpers.ts

--- src/utils/compoundApyHelpers.ts
+++ src/utils/compoundApyHelpers.ts
@@ -1,18 +1,18 @@
 const roundToTwoDp = (number) => Math.round(number * 100) / 100
 
-export const calculateCakeEarnedPerThousandDollars = ({ numberOfDays, farmApy, cakePrice }) => {
+export const calculatePieEarnedPerThousandDollars = ({ numberOfDays, farmApy, piePrice }) => {
   // Everything here is worked out relative to a year, with the asset compounding daily
   const timesCompounded = 365
   //   We use decimal values rather than % in the math for both APY and the number of days being calculates as a proportion of the year
   const apyAsDecimal = farmApy / 100
   const daysAsDecimalOfYear = numberOfDays / timesCompounded
-  //   Calculate the starting CAKE balance with a dollar balance of $1000.
-  const principal = 1000 / cakePrice
+  //   Calculate the starting PIE balance with a dollar balance of $1000.
+  const principal = 1000 / piePrice
 
   // This is a translation of the typical mathematical compounding APY formula. Details here: https://www.calculatorsoup.com/calculators/financial/compound-interest-calculator.php
   const finalAmount = principal * (1 + apyAsDecimal / timesCompounded) ** (timesCompounded * daysAsDecimalOfYear)
 
-  // To get the cake earned, deduct the amount after compounding (finalAmount) from the starting CAKE balance (principal)
+  // To get the pie earned, deduct the amount after compounding (finalAmount) from the starting PIE balance (principal)
   const interestEarned = finalAmount - principal
   return roundToTwoDp(interestEarned)
 }
