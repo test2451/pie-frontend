ba4d630edb5fd5e43fe21abeb9365adad817fcdf
test2451 <79050952+test2451@users.noreply.github.com>
2021-02-16 16:43:03 +0530

    Update fetchPoolsUser.ts

--- src/state/pools/fetchPoolsUser.ts
+++ src/state/pools/fetchPoolsUser.ts
@@ -9,10 +9,10 @@ import { getAddress, getMasterChefAddress } from 'utils/addressHelpers'
 import { getWeb3 } from 'utils/web3'
 import BigNumber from 'bignumber.js'
 
-// Pool 0, Cake / Cake is a different kind of contract (master chef)
+// Pool 0, Pie / Pie is a different kind of contract (master chef)
 // BNB pools use the native BNB token (wrapping ? unwrapping is done at the contract level)
-const nonBnbPools = poolsConfig.filter((p) => p.stakingTokenName !== QuoteToken.BNB)
-const bnbPools = poolsConfig.filter((p) => p.stakingTokenName === QuoteToken.BNB)
+const nonBnbPools = poolsConfig.filter((p) => p.stakingTokenName !== QuoteToken.OKT)
+const oktPools = poolsConfig.filter((p) => p.stakingTokenName === QuoteToken.OKT)
 const nonMasterPools = poolsConfig.filter((p) => p.sousId !== 0)
 const web3 = getWeb3()
 const masterChefContract = new web3.eth.Contract((masterChefABI as unknown) as AbiItem, getMasterChefAddress())
@@ -32,7 +32,7 @@ export const fetchPoolsAllowance = async (account) => {
 }
 
 export const fetchUserBalances = async (account) => {
-  // Non BNB pools
+  // Non OKT pools
   const calls = nonBnbPools.map((p) => ({
     address: p.stakingTokenAddress,
     name: 'balanceOf',
@@ -44,14 +44,14 @@ export const fetchUserBalances = async (account) => {
     {},
   )
 
-  // BNB pools
+  // OKT pools
   const bnbBalance = await web3.eth.getBalance(account)
   const bnbBalances = bnbPools.reduce(
     (acc, pool) => ({ ...acc, [pool.sousId]: new BigNumber(bnbBalance).toJSON() }),
     {},
   )
 
-  return { ...tokenBalances, ...bnbBalances }
+  return { ...tokenBalances, ...oktBalances }
 }
 
 export const fetchUserStakeBalances = async (account) => {
@@ -69,7 +69,7 @@ export const fetchUserStakeBalances = async (account) => {
     {},
   )
 
-  // Cake / Cake pool
+  // Pie / Pie pool
   const { amount: masterPoolAmount } = await masterChefContract.methods.userInfo('0', account).call()
 
   return { ...stakedBalances, 0: new BigNumber(masterPoolAmount).toJSON() }
@@ -90,8 +90,8 @@ export const fetchUserPendingRewards = async (account) => {
     {},
   )
 
-  // Cake / Cake pool
-  const pendingReward = await masterChefContract.methods.pendingCake('0', account).call()
+  // Pie / Pie pool
+  const pendingReward = await masterChefContract.methods.pendingPie('0', account).call()
 
   return { ...pendingRewards, 0: new BigNumber(pendingReward).toJSON() }
 }
